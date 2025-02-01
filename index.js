42aa8e71d9f3ecc9f2fc01ac0cadc3302ab344e2
Austin <austin.moorer@gmail.com>
2025-02-01 16:27:03 -0500

    Added required packages, initial auth code added to index

---
+++ index.js
@@ -0,0 +1,78 @@
+const path = require("node:path");
+const express = require("express");
+const session = require("express-session");
+const { PrismaClient } = require("@prisma/client");
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
+const { body, validationResult } = require("express-validator");
+require("dotenv").config();
+
+const prisma = new PrismaClient();
+
+const app = express();
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(
+  session({
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000, //ms
+    },
+    secret: process.env.secret,
+    resave: false,
+    saveUninitialized: false,
+    store: new PrismaSessionStore(new PrismaClient(), {
+      checkPeriod: 2 * 60 * 1000,
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  }),
+);
+
+passport.use(
+  new LocalStrategy(async (email, password, done) => {
+    try {
+      const user = await prisma.user.findUnique({
+        where: {
+          email: email,
+        },
+      });
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  }),
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await prisma.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    done(null, user);
+  } catch {
+    console.log(err);
+    done(err);
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send(err);
+});
